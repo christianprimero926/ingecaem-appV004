a661e146a13398f9fb5b9d4a50fc8a52190dae59
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-27 19:01:19 -0500

    Initial Commit

---
+++ src/main/java/com/cospina/ingecaem/app/IngecaemAppV004Application.java
@@ -0,0 +1,13 @@
+package com.cospina.ingecaem.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngecaemAppV004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngecaemAppV004Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cospina/ingecaem/app/IngecaemAppV004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cospina.ingecaem.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngecaemAppV004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
